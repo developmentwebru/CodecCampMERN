b2b54479cc49060ad7318c15a372f5f8af341044
sergeisaz <sergeisaz53@gmail.com>
2021-02-17 01:42:17 +0300

    first message

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const cokieParser = require('cookie-parser')
+
+const errorMiddleware = require('./middlewares/errors')
+
+app.use(express.json());
+app.use(cokieParser())
+
+// import all routes
+const products = require('./routes/product')
+const auth = require('./routes/auth')
+
+app.use('/api/v1', products)
+app.use('/api/v1', auth)
+//Middleware to handle errors
+app.use(errorMiddleware);
+
+module.exports = app
