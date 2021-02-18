1149c444bf56b07a8119b5c9ce0e91f5b4ec7350
sergeisaz <sergeisaz53@gmail.com>
2021-02-18 17:49:02 +0300

    delete review

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -11,7 +11,8 @@ const {
     updateProduct,
     deleteProduct,
     createProductReview,
-    getProductReviews
+    getProductReviews,
+    deleteReview
 
 } = require('../controllers/productController')
 
@@ -30,5 +31,6 @@ router.route('/admin/product/:id')
 
 router.route('/review').put(isAuthenticatedUser, createProductReview)
 router.route('/reviews').get(isAuthenticatedUser, getProductReviews)
+router.route('/reviews').delete(isAuthenticatedUser, deleteReview)
 
 module.exports = router;
