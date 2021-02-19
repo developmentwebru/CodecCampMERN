569954e62b43417414a91066d89bc677f99a3297
sergeisaz <sergeisaz53@gmail.com>
2021-02-19 14:10:48 +0300

    add productDetailsReducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,7 +2,10 @@ import {
     ALL_PRODUCTS_REQUEST,
     All_PRODUCTS_SUCCESS,
     All_PRODUCTS_FAIL,
-    CLEAR_ERRORS
+    CLEAR_ERRORS,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants'
 
 export const productsReducer = (state = { products: [] }, action) => {
@@ -24,6 +27,37 @@ export const productsReducer = (state = { products: [] }, action) => {
                 loading: false,
                 error: action.payload
             }
+        case CLEAR_ERRORS:
+
+            return {
+                ...state,
+                error: null
+            }
+
+        default:
+            return state;
+    }
+}
+
+
+export const productDetailsReducer = (state = { product: {} }, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case PRODUCT_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                product: action.payload
+            }
+        case PRODUCT_DETAILS_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
+
         case CLEAR_ERRORS:
 
             return {
