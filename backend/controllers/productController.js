14d18f1bab4a554a799747bdeb496a4fd024470c
sergeisaz <sergeisaz53@gmail.com>
2021-02-19 11:10:35 +0300

    add productsReducers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -31,7 +31,6 @@ exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
     res.status(200).json({
         success: true,
-        count: products.length,
         productCount,
         products
     })
