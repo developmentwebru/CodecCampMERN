08bdda14558d5c172edb1cfd9366feb801d365a3
sergeisaz <sergeisaz53@gmail.com>
2021-02-20 17:33:53 +0300

    get filter category

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,20 +21,25 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
 
-
-    const resPerPage = 8;
+    const resPerPage = 1;
     const productsCount = await Product.countDocuments();
 
     const apiFeatures = new APIFeatures(Product.find(), req.query)
         .search()
         .filter()
-        .pagination(resPerPage)
-    const products = await apiFeatures.query;
+
+    let products = await apiFeatures.query;
+    let filteredProductsCount = products.length;
+    
+    apiFeatures.pagination(resPerPage)
+    products = await apiFeatures.query;
 
 
     res.status(200).json({
         success: true,
         productsCount,
+        resPerPage,
+        filteredProductsCount,
         products
     })
 
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import MetaData from './layout/MetaData';
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
 import Product from './product/Product';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../actions/productAction'
@@ -7,14 +9,37 @@ import Loader from './layout/Loader'
 import { useAlert } from 'react-alert'
 import Pagination from 'react-js-pagination'
 
+
+
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range)
+
+
 const Home = ({ match }) => {
     const [currentPage, setCurrentPage] = useState(1)
+    const [price, setPrice] = useState([1, 1000])
+    const [category, setCategory] = useState('')
+
+    const categories = [
+        'Electronics',
+        'Cameras',
+        'Laptop',
+        'Accessories',
+        'Headphones',
+        'Food',
+        'Books',
+        'Clothes/Shoes',
+        'Beauty/Helth',
+        'Sports',
+        'Outdoor',
+        'Home'
+    ]
 
     const alert = useAlert();
 
     const dispatch = useDispatch();
 
-    const { loading, products, error, productsCount, resPerPage } = useSelector(state => state.products)
+    const { loading, products, error, productsCount, resPerPage, filteredProductsCount } = useSelector(state => state.products)
 
     const keyword = match.params.keyword
 
@@ -22,14 +47,18 @@ const Home = ({ match }) => {
         if (error) {
             return alert.error(error)
         }
-        dispatch(getProducts(keyword, currentPage));
+        dispatch(getProducts(keyword, currentPage, price, category));
 
 
 
-    }, [dispatch, alert, error, keyword, currentPage])
+    }, [dispatch, alert, error, keyword, currentPage, price, category])
 
     function setCurrentPageNo(pageNumber) {
         setCurrentPage(pageNumber)
+    }
+    let count = productsCount;
+    if (keyword) {
+        count = filteredProductsCount
     }
 
     return (
@@ -41,14 +70,70 @@ const Home = ({ match }) => {
 
                     <section id="products" className="container mt-5">
                         <div className='row'>
-                            {products && products.map(product => (
-                                <Product key={product._id} product={product} />
-                            ))}
+
+                            {keyword ? (
+                                <Fragment>
+                                    <div className="col-6 col-md-3 mt-5 mb-5">
+                                        <div className="px-5">
+                                            <Range
+                                                marks={{
+                                                    1: `$1`,
+                                                    1000: `$1000`
+                                                }}
+                                                min={1}
+                                                max={1000}
+                                                defaultValue={[1, 1000]}
+                                                tipFormatter={value => `$${value}`}
+                                                tipProps={{
+                                                    placement: "top",
+                                                    visible: true
+                                                }}
+                                                value={price}
+                                                onChange={price => setPrice(price)}
+                                            />
+                                            <hr className="my-5" />
+                                            <div className="mt-5">
+                                                <h4 className="mb-3">
+                                                    Categories
+                                                </h4>
+                                                <ul className="pl-0">
+                                                    {categories.map(category => (
+                                                        <li style={{
+                                                            cursor: 'pointer',
+                                                            listStyleType: 'none'
+                                                        }}
+                                                            key={category}
+                                                            onClick={() => setCategory(category)}
+                                                        >
+                                                            {category}
+                                                        </li>
+                                                    ))}
+
+                                                </ul>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div className="col-6 col-md-p">
+                                        <div className="row">
+                                            {products && products.map(product => (
+                                                <Product key={product._id} product={product} col={4} />
+                                            ))}
+                                        </div>
+                                    </div>
+                                </Fragment>
+                            ) : (
+                                    products && products.map(product => (
+                                        <Product key={product._id} product={product} col={3} />
+                                    ))
+                                )}
+
+
 
                         </div>
 
                     </section>
-                    {resPerPage < productsCount && (
+                    {resPerPage < count && (
                         <div className="d-flex justify-content-center mt-5">
                             <Pagination
                                 active={currentPage}
