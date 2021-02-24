e48c833739700992912ff1c3fb9bcee48e1580e3
sergeisaz <sergeisaz53@gmail.com>
2021-02-25 00:25:22 +0300

    add delete user in dashboard ADMIN

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -266,6 +266,9 @@ exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
         return next(new ErrorHandler(`User does not found with id:${req.params.id}`))
     }
     //Remove aeater from cloudinary - TODO
+    const image_id = user.avatar.public_id;
+    await cloudinary.v2.uploader.destroy(image_id)
+
     await user.remove();
 
     res.status(200).json({
